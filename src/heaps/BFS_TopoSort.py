53ebdfa95d64f9e354cf9f8d5c5b6935dd3ce337
Niranjan Agnihotri <niranjan.sa.cs@gmail.com>
2021-09-06 20:20:27 -0700

    Create BFS_TopoSort.py

---
+++ src/heaps/BFS_TopoSort.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minimumSemesters(self, N: int, relations) -> int:
+        """
+        if all the vertices are not touched then there is a cycle and we exit. 
+        O(N+E) in Time & Space where N is no of vertices & E is edges
+        """
+        graph = {i: [] for i in range(1, N + 1)}
+        in_count = {i: 0 for i in range(1, N + 1)}  # or in-degree
+        for start_node, end_node in relations:
+            graph[start_node].append(end_node)
+            in_count[end_node] += 1
+
+        queue = []
+        # we use list here since we are not
+        # poping from front the this code
+        for node in graph:
+            if in_count[node] == 0:
+                queue.append(node)
+
+        step = 0
+        studied_count = 0
+        # start learning with BFS
+        while queue:
+            # start new semester
+            step += 1
+            next_queue = []
+            for node in queue:
+                studied_count += 1
+                end_nodes = graph[node]
+                for end_node in end_nodes:
+                    in_count[end_node] -= 1
+                    # if all prerequisite courses learned
+                    if in_count[end_node] == 0:
+                        next_queue.append(end_node)
+            queue = next_queue
+
+        return step if studied_count == N else -1
