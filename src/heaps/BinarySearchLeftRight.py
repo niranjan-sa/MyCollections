30977f1e641c57923b9c431ae1d8b18c58303d64
Niranjan Agnihotri <niranjan.sa.cs@gmail.com>
2021-10-27 22:16:29 -0700

    Create BinarySearchLeftRight.py

---
+++ src/heaps/BinarySearchLeftRight.py
@@ -0,0 +1,28 @@
+"""
+Needs at least more than 3 or more elements.
+Use when you need to access left and right elements
+"""
+def binarySearch(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    if len(nums) == 0:
+        return -1
+
+    left, right = 0, len(nums) - 1
+    while left + 1 < right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid
+        else:
+            right = mid
+
+    # Post-processing:
+    # End Condition: left + 1 == right
+    if nums[left] == target: return left
+    if nums[right] == target: return right
+    return -1
