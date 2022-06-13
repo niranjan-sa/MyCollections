b2d060339a4c0ce45d415ca66e811dfb75f2b0ed
Niranjan Agnihotri <niranjan.sa.cs@gmail.com>
2022-06-12 20:45:01 -0700

    Update kadane.py

--- src/heaps/kadane.py
+++ src/heaps/kadane.py
@@ -1,5 +1,17 @@
 """
 Find the maximum sub array sum in an array with -ve +ve elements.
+
+Initialize:
+    max_so_far = INT_MIN
+    max_ending_here = 0
+
+Loop for each element of the array
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+  (c) if(max_ending_here < 0)
+            max_ending_here = 0
+return max_so_far
 """
 
 def kadane(nums):
